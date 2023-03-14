95394f2f7332060f2f72c7f94cecd6a508c93c98
thomasbove <z5160565@unsw.edu.au>
2023-03-15 10:41:03 +1100

    Generated good looking graphs

--- graph/plot.py
+++ graph/plot.py
@@ -3,11 +3,12 @@ import matplotlib
 matplotlib.use('Agg') # no UI backend
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 np.set_printoptions(threshold=np.inf)
 import sys
 
 if __name__ ==  "__main__":
-    file = open("../cva6-fencet-btb-invoke", "r")
+    file = open("../cva6-fencet-l1i-invoke", "r")
     x = []
     y = []
 
@@ -19,9 +20,12 @@ if __name__ ==  "__main__":
         x.append(int(split[0]))
         y.append(int(split[1]))
 
-    a = np.zeros(shape=(2*max(y), max(x)+1), dtype="int32")
+    a = np.zeros(shape=(max(y)+50000, max(x)+1), dtype="int32")
     for (secret, time) in zip(x, y):
         a[time][secret] += 1
+
+    # half = math.floor(len(a)/2)
+    a = a[48000:48700][:]
 
     plt.imshow(a, interpolation='nearest', aspect='auto')
     ax = plt.gca()
@@ -29,5 +33,12 @@ if __name__ ==  "__main__":
     plt.colorbar(label='Frequency')
     plt.xlabel("Secret")
     plt.ylabel("Time (Cycles)")
+    # plt.yticks([0, 10000, 20000, 30000, 40000, 50000, 60000], [0+half, 10000+half, 20000+half, 30000+half, 40000+half, 50000+half, 60000+half])
+    # plt.yticks([0, 100, 200, 300, 400], [0+136500, 100+136500, 200+136500, 300+136500, 400+136500])
+    # plt.yticks([0, 50, 100, 150, 200], [0+900, 50+900, 100+900, 150+900, 200+900])
+    # plt.yticks([0, 50, 100, 150, 200, 250], [0+3100, 50+3100, 100+3100, 150+3100, 200+3100, 250+3100])
+    # plt.yticks([0, 50, 100, 150, 200], [0+1000, 50+1000, 100+1000, 150+1000, 200+1000])
+    # plt.yticks([0, 2000, 4000, 6000, 8000, 10000, 12000, 14000, 16000], [0+30000, 2000+30000, 4000+30000, 6000+30000, 8000+30000, 10000+30000, 12000+30000, 14000+30000, 16000+30000])
+    plt.yticks([0, 100, 200, 300, 400, 500, 600], [0+48000, 100+48000, 200+48000, 300+48000, 400+48000, 500+48000, 600+48000])
 
-    plt.savefig("cva6-fencet-btb-invoke.png")
+    plt.savefig("cva6-fencet-l1i-invoke.png")
