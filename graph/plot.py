5a5f073d2f5a72bd7fe915c5feccc749431bb79b
tom <tombove2035@gmail.com>
2023-06-17 18:48:32 +1000

    Fixed borken plot code, got temporal fence with no kernel clone graphs

--- graph/plot.py
+++ graph/plot.py
@@ -21,22 +21,20 @@ if __name__ ==  "__main__":
     # Open the input file
     file = open(inputFile, "r")
 
-    # Construct 2D matrix
-    x = []
-    y = []
+    ar = []
     while True:
         line = file.readline()
         if not line:
             break
         split = line.split(" ")
-        x.append(int(split[0]))
-        y.append(int(split[1]))
-    a = np.zeros(shape=(max(y)+50000, max(x)+1), dtype="int32")
-    for (secret, time) in zip(x, y):
-        a[time][secret] += 1
-    # The following is for cutting the channel matrix to remove large areas of empty data
-    # half = math.floor(len(a)/2)
-    a = a[18000:21000][:]
+        ar.append((int(split[0]), int(split[1])))
+
+    maxTime = max(ar, key=lambda item: item[1])[1]+5000
+    maxSecret = max(ar, key=lambda item: item[0])[0]+1
+    a = np.zeros(shape=(maxTime, maxSecret), dtype="int32")
+    for (x,y) in ar:
+        a[y][x] += 1
+    a = a[27000:30000][:]
 
     # Construct graph
     plt.imshow(a, interpolation='nearest', aspect='auto')
@@ -46,8 +44,9 @@ if __name__ ==  "__main__":
     plt.xlabel("Secret")
     plt.ylabel("Time (Cycles)")
 
-    plt.yticks([0, 500, 1000, 1500, 2000, 2500], [0+150, 500+150, 1000+150, 1500+150, 2000+150, 2500+150])
-    # plt.yticks([0, 500, 1000, 1500, 2000, 2500], [0+27000, 500+27000, 1000+27000, 1500+27000, 2000+27000, 2500+27000])
+    # plt.yticks([0, 25, 50, 75, 100, 125, 150, 175], [0+900, 25+900, 50+900, 75+900, 100+900, 125+900, 150+900, 175+900])
+    # plt.yticks([0, 20, 40, 60, 80], [0+300, 20+300, 40+300, 60+300, 80+300])
+    plt.yticks([0, 500, 1000, 1500, 2000, 2500], [0+27000, 500+27000, 1000+27000, 1500+27000, 2000+27000, 2500+27000])
     plt.xticks([-0.5, 0.5, 1.5], [0, 1, 2])
 
     # Output graph
